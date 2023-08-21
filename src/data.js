bdb84d80e55f6d75e00391ec324d5ccb0ce7bd2b
sayalibadole <badolesayali@gmail.com>
2023-08-20 18:46:54 -0700

    faq

---
+++ src/data.js
@@ -0,0 +1,33 @@
+const questions = [
+  {
+    id: 1,
+    title: 'What is internet safety, and why is it important?',
+    info:
+      'Internet safety, also known as online safety or cybersecurity, refers to the practices and measures taken to protect yourself and your personal information while using the internet. Its important because the internet can expose you to various risks, including cyberattacks, identity theft, and online harassment. Practicing internet safety helps you stay secure and maintain your privacy online.',
+  },
+  {
+    id: 2,
+    title: 'How can I protect my personal information online?',
+    info:
+      'Strong Passwords: Use complex passwords for your accounts and avoid using the same password across multiple sites.\nTwo-Factor Authentication (2FA): Enable 2FA when available to add an extra layer of security.\nPrivacy Settings: Adjust privacy settings on social media platforms and other online accounts to limit who can access your information.\nAvoid Phishing: Be cautious of unsolicited emails or messages asking for personal information. Verify the senders legitimacy before responding.',
+  },
+  {
+    id: 3,
+    title: 'What is phishing, and how can I avoid falling for it?',
+    info:
+      'Phishing is a type of cyberattack where attackers impersonate a legitimate entity to trick you into revealing sensitive information, such as passwords or credit card numbers. To avoid falling for phishing: \nBe Skeptical: Dont click on links or download attachments from unknown sources.\nVerify URLs: Hover over links to see the actual website address before clicking.\nCheck Sender: Ensure the email senders address matches the official one.\nUse Security Software: Install reputable antivirus and anti-malware software to detect phishing attempts.',
+  },
+  {
+    id: 4,
+    title: 'How can I stay safe on social media?',
+    info:
+      'To stay safe on social media:\nLimit Sharing: Avoid sharing personal information like your address, phone number, or full birthdate.\nCheck Privacy Settings: Set your profiles to private and review who can see your posts.\nBe Selective: Only accept friend/follower requests from people you know.\nThink Before Posting: Consider the potential impact of your posts before sharing.',
+  },
+  {
+    id: 5,
+    title: 'What should I do if I am a victim of cyberbullying?',
+    info:
+      'If you are a victim of online harassment or cyberbullying.\nDocument Evidence: Take screenshots or save copies of harassing messages or posts.\nBlock and Report: Block the harasser and report their behavior to the platform administrators. \nSeek Support: Talk to friends, family, or a trusted adult about the situation. \nContact Authorities: If the harassment escalates or becomes threatening, consider involving law enforcement.',
+  },
+]
+export default questions
